0f495e69f1ac01abd799f018d5c9039521a1559d
SomeQ <thesomeq@gmail.com>
2015-01-21 12:18:21 +0600

    #50 modified site details, added survey toggles

--- bigsurvey/webapp/templatetags/surveys_for_services.py
+++ bigsurvey/webapp/templatetags/surveys_for_services.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 @register.inclusion_tag('survey_list.html')
 def include_surveys(site, service_type):
-    service = site.services.filter(service_type__service_type=service_type)
-    return {'surveys': service.surveys}
+    service = site.services.filter(service_type__service_type=service_type)[0]
+    return {'surveys': service.surveys.all()}
