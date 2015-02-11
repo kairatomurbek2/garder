a6960c7582c6aa61e9eb12b627edced30c02f2d3
SomeQ <thesomeq@gmail.com>
2015-02-11 13:37:21 +0600

    #91 changed wsgi.py

--- bigsurvey/main/wsgi.py
+++ bigsurvey/main/wsgi.py
@@ -8,6 +8,14 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+site.addsitedir(BASE_DIR+'/../virtualenv/lib/python2.7/site-packages')
+sys.path.append(BASE_DIR)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
 from django.core.wsgi import get_wsgi_application
