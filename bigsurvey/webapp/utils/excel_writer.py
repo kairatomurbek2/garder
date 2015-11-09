6041cfe7b94ec2208397aff9e131a1631859d8b4
Farid <thesomeq@gmail.com>
2015-11-09 16:59:45 +0600

    #230 Изменил формат файла с xls на xlsx, поставил соответствующий питоновский пакет в зависимости. Причина - xls не может иметь более 65536 рядов, а в базе записей гораздо больше. Теоретически, можно будет в дальнейшем сделать перенос данных на новые листы

--- bigsurvey/webapp/utils/excel_writer.py
+++ bigsurvey/webapp/utils/excel_writer.py
@@ -1,6 +1,6 @@
-from xlwt import Workbook, XFStyle, Font
+from xlsxwriter import Workbook
 from django.utils.translation import ugettext as _
-from main.parameters import SITE_FIELD_NAMES, SITE_BOOLEAN_FIELDS, SITE_DATE_FIELDS, YESNO_CHOICES
+from main.parameters import SITE_FIELD_NAMES, SITE_BOOLEAN_FIELDS, SITE_DATE_FIELDS
 from main.settings import BASE_DIR
 from datetime import datetime
 import os
@@ -8,11 +8,9 @@ import os
 
 class XLSExporter(object):
     def __init__(self, dataset):
-        self.file_name = os.path.join(BASE_DIR, 'uploads/excel_export/export_%s.xls' % datetime.now().strftime("%s"))
-        self.xls_file = open(self.file_name, 'w')
-        self.workbook = Workbook(encoding="utf-8")
-        self.current_sheet = self.workbook.add_sheet(_("Report"))
-        self.header_style = self._get_header_style()
+        self.file_name = os.path.join(BASE_DIR, 'uploads/excel_export/export_%s.xlsx' % datetime.now().strftime("%s"))
+        self.workbook = Workbook(self.file_name)
+        self.current_sheet = self.workbook.add_worksheet(_("Sites"))
         self.dataset = dataset
         self.fields = SITE_FIELD_NAMES
 
@@ -21,17 +19,11 @@ class XLSExporter(object):
         self._write_data()
         return self.file_name
 
-    def _get_header_style(self):
-        style = XFStyle()
-        font = Font()
-        font.bold = True
-        style.font = font
-        return style
-
     def _write_headers(self):
         col = 0
+        bold = self.workbook.add_format({'bold': True})
         for field in self.fields:
-            self.current_sheet.write(0, col, field, self.header_style)
+            self.current_sheet.write(0, col, field, bold)
             col += 1
 
     def _write_data(self):
@@ -44,8 +36,7 @@ class XLSExporter(object):
                 self.current_sheet.write(row, col, value)
                 col += 1
             row += 1
-        self.workbook.save(self.xls_file)
-        self.xls_file.close()
+        self.workbook.close()
 
     def _get_field_value(self, field, field_name):
         if field is None:
