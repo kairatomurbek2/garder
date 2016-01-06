c7385f3c69e199ff5cad9e2db29df55a22a120b7
kayrat <kairatomurbek2@gmail.com>
2016-01-06 16:27:46 +0600

    #263 Добавил данные в  Regulation Numbers из MiniApplication

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd bigsurvey
 ./manage.py migrate --noinput
 FILE_PATH="./webapp/fixtures/raw/"
 FILE_PREFIX="raw_data_"
-for data_type in "base" "pws" "perms" "users" "testers" "site" "hazard" "survey" "test_kit" "tester_cert" "test" "letter"
+for data_type in "base" "pws" "perms" "users" "testers" "site" "hazard" "survey" "test_kit" "tester_cert" "test" "letter" "regulation"
 do
     for part_number in 0 `seq 50`
     do
--- load_raw_data.sh
+++ load_raw_data.sh
@@ -4,7 +4,7 @@ set -e
 cd ./bigsurvey/
 FILE_PATH="./webapp/fixtures/raw/"
 FILE_PREFIX="raw_data_"
-for data_type in "base" "pws" "perms" "users" "testers" "site" "hazard" "survey" "test_kit" "tester_cert" "test" "letter"
+for data_type in "base" "pws" "perms" "users" "testers" "site" "hazard" "survey" "test_kit" "tester_cert" "test" "letter" "regulation"
 do
     for part_number in `seq 0 100`
     do
