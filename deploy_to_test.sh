8e308f6fd2aca7d238193623fed91cafa2b054d5
murat <jumasheff@gmail.com>
2016-01-08 10:55:48 +0600

    #268 переделал скрипт deploy_to_test.sh, чтобы он принимал номер билда, лепил к нему префикс из файла version_source, и передавал полученный тег  фаб файлу

--- deploy_to_test.sh
+++ deploy_to_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 . virtualenv/bin/activate
-fab demo_env deploy_demo:$1
+# $1 here is Jenkins build number that you should pass to this file from command line
+TAG="$(read -r version < version_source; echo ${version//\*/$1})"
+fab demo_env deploy_demo:${TAG}
 deactivate
