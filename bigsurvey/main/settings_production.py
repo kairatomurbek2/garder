7385a2d857779157283345be6ed17b0672434714
kayrat <kairatomurbek2@gmail.com>
2016-01-15 10:57:43 +0600

    #283 Подключил PayPal? настройки на production

--- bigsurvey/main/settings_production.py
+++ bigsurvey/main/settings_production.py
@@ -14,3 +14,7 @@ ALLOWED_HOSTS = [
 ]
 
 HOST = 'http://bss.bpsbr.com'
+
+PAYPAL_MODE = 'live'
+PAYPAL_CLIENT_ID = 'AVZyLVR5fCHOkBg6DV1nngIHwOslvh6jcjE3eX1aFooPineCUITsJzcixOpqXoBxyZDO38GdocDVFI1f'
+PAYPAL_CLIENT_SECRET = 'EJVZ7Zlc7SgoBTsK7lbDBPiVIFmBgxIKFv1FcEetpKlaydhw2HchpLXqsRypD6KaqM4zjQ3FwzFpg6Z5'
