e3be45f34d2c874f3086170649d74803631a5952
murat <jumasheff@gmail.com>
2016-02-01 10:39:00 +0600

    #319 Реализовал функционал отображения кнопки Add PWS только тем, у кого есть право на создание pws

--- bigsurvey/webapp/templatetags/common.py
+++ bigsurvey/webapp/templatetags/common.py
@@ -16,3 +16,8 @@ def resolve(target, lookup):
 @register.filter()
 def verbose_name(object):
     return object._meta.verbose_name
+
+
+@register.filter(name="has_access")
+def has_access(user, access):
+    return user.has_perm("webapp.%s" % access)
