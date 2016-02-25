6be0814fd815c28ef11b9f291e877e3891d967cc
kayrat <kairatomurbek2@gmail.com>
2016-02-25 11:38:19 +0600

    #283 Добавил ключи paypal для demo

--- bigsurvey/main/settings_demo.py
+++ bigsurvey/main/settings_demo.py
@@ -1,3 +1,5 @@
+import paypalrestsdk
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -12,6 +14,16 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'bfp-services.ltestl.com',
 ]
+PAYPAL_MODE = 'live'
+PAYPAL_CLIENT_ID = 'AVZyLVR5fCHOkBg6DV1nnglHwOslvh6jcjE3eX1aFooPineCUITsJzcixOpqXoBxyZDO38GdocDVFl1f'
+PAYPAL_CLIENT_SECRET = 'EJVZ7Zlc7SgoBTsK7lbDBPiVlFmBgxlKFv1FcEetpKIaydhw2HchpLXqsRypD6KaqM4zjQ3FwzFpg6Z5'
+
+paypalrestsdk.configure({
+    'mode': PAYPAL_MODE,
+    'client_id': PAYPAL_CLIENT_ID,
+    'client_secret': PAYPAL_CLIENT_SECRET,
+})
+
 
 HOST = 'http://bfp-services.ltestl.com'
 
