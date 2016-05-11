cbabe014d409295635d25881f4cd24eb5c346e46
kayrat <kairatomurbek2@gmail.com>
2016-05-11 11:40:12 +0600

    #352 Реализовал проверку, что если пользователь относится к группе Administrative Authority, то перенапрвлять его на страницу pws_list

--- bigsurvey/accounts/views.py
+++ bigsurvey/accounts/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import login, logout
 from django.views.generic import View
 from django.core.urlresolvers import reverse
 
+from main.parameters import Groups
+
 
 class LogoutView(View):
     def get(self, request):
@@ -18,6 +20,8 @@ class LoginView(View):
 
     def get(self, request):
         if self.request.user.is_authenticated():
+            if request.user.groups.filter(name=Groups.ad_auth):
+                return HttpResponseRedirect(reverse('webapp:pws_list'))
             return HttpResponseRedirect(request.GET.get('next', reverse('webapp:home')))
         form = self.form_class(request)
         return render(request, self.template_name, {'form': form})
@@ -26,5 +30,7 @@ class LoginView(View):
         form = self.form_class(request, data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
+            if request.user.groups.filter(name=Groups.ad_auth):
+                return HttpResponseRedirect(reverse('webapp:pws_list'))
             return HttpResponseRedirect(request.GET.get('next', reverse('webapp:home')))
         return render(request, self.template_name, {'form': form})
