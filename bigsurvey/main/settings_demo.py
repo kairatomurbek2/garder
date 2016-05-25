b1b3d7f1207de5fe5897e46ed5d76c9618c28340
kayrat <kairatomurbek2@gmail.com>
2016-05-25 09:21:12 +0600

    #364 Перевел PayPal на демо-версии в sandbox-режим

--- bigsurvey/main/settings_demo.py
+++ bigsurvey/main/settings_demo.py
@@ -1,5 +1,3 @@
-import paypalrestsdk
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -14,16 +12,6 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'bfp-services.ltestl.com',
 ]
-PAYPAL_MODE = 'live'
-PAYPAL_CLIENT_ID = 'AVZyLVR5fCHOkBg6DV1nnglHwOslvh6jcjE3eX1aFooPineCUITsJzcixOpqXoBxyZDO38GdocDVFl1f'
-PAYPAL_CLIENT_SECRET = 'EJVZ7Zlc7SgoBTsK7lbDBPiVlFmBgxlKFv1FcEetpKIaydhw2HchpLXqsRypD6KaqM4zjQ3FwzFpg6Z5'
-
-paypalrestsdk.configure({
-    'mode': PAYPAL_MODE,
-    'client_id': PAYPAL_CLIENT_ID,
-    'client_secret': PAYPAL_CLIENT_SECRET,
-})
-
 
 HOST = 'http://bfp-services.ltestl.com'
 
