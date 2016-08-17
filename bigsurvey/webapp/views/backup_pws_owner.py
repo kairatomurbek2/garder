612e95edc721fc0bb8106eee7034d043fcfb0c8b
kayrat <kairatomurbek2@gmail.com>
2016-08-17 15:24:47 +0600

    #377 Реализовал код  чтоб не показывать страницу backup для PWSOwner's во время demo trial

--- bigsurvey/webapp/views/backup_pws_owner.py
+++ bigsurvey/webapp/views/backup_pws_owner.py
@@ -1,11 +1,12 @@
 import os
 from subprocess import call
-
 import shutil
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
 from django.forms import ModelChoiceField
 from main.parameters import Messages
 from webapp import forms, models
+from webapp.actions.demo_trial import IsEmployeeInTrialPeriod
 from webapp.views import BaseFormView, reverse, HttpResponseRedirect
 from django.contrib import messages
 
@@ -16,6 +17,13 @@ class BackupPWSOwner(BaseFormView):
     template_name = 'backup.html'
     form_class = forms.BackupPWSOwnerForm
     model = models.Backup
+
+    def get_context_data(self, **kwargs):
+        context = super(BackupPWSOwner, self).get_context_data(**kwargs)
+        if not self._user_is_in_demo_trial():
+            context['is_demo_trial'] = False
+            return context
+        raise PermissionDenied
 
     def get_form(self, form_class):
         form = super(BackupPWSOwner, self).get_form(form_class)
@@ -45,6 +53,9 @@ class BackupPWSOwner(BaseFormView):
                                                    datetime__gte=backup.time_stamp)
         for import_log in queryset:
             import_log.added_sites.update(status=models.SiteStatus.objects.get(site_status='Inactive'))
+
+    def _user_is_in_demo_trial(self):
+        return IsEmployeeInTrialPeriod.check(self.request.user)
 
     def get_success_url(self):
         return reverse('webapp:backup_pws')
